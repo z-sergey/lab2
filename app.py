88f8ece7986ce1fdeaa4a1f44772330c196b472a
z_sergey <zyryanov.sergey.1988@mail.ru>
2016-06-06 16:49:53 +0000

    kurs

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
-import argparse
-import datetime
-import logging
+import sys 
+import argparse 
+import process 
+import logging 
+import datetime 
 
-import process as dp
 
-if __name__ == '__main__':
-    logging.basicConfig(filename='app.log', level=logging.INFO, filemode='w')
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser.add_argument('symbol', help='Символ финансового инструмента')
-    parser.add_argument('-y', '--year', help='Год для анализа')
-    parser.add_argument('-p', '--print', help='Вывести все данные',
-                        action='store_true')
-    args = parser.parse_args()
+def createParser (): 
+    
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("-s", "--symbol", help="Биржевой символ акций") 
+    parser.add_argument("-f","--file", help="Файл для обработки") 
+    parser.add_argument ("-u", "--up_value", help="Максимальный объем продаж", type=float, default=0) 
+    parser.add_argument ("-y", "--year", help="Год", default=datetime.datetime.now().year, type=int) 
+    parser.add_argument ("-fo", "--file_out", help="Файл для записи результата", default="rezult.txt") 
+    parser.add_argument ("-fl", "--file_log", help="Файл для логирования", default="app.log") 
+    parser.add_argument ("-l", "--level_log", help="Уровень логирования", default="INFO") 
+    parser.add_argument ("-i", "--indicator", help="Индикатор (MFI-Индекс денежного потока)") 
+    
+    return parser 
 
-    # Определяем год для чтения данных
-    # Если год не указан берем текущий год
-    if args.year:
-        year = int(args.year)
-    else:
-        year = datetime.datetime.now().year
-    logging.debug('Год {:d}'.format(year))
-    start_date = datetime.date(year, 1, 1)
-    end_date = datetime.date(year, 12, 31)
-
-    logging.debug('Загрузка данных для ' + args.symbol)
-    data = dp.google(args.symbol, start_date, end_date)
-
-    sma = None
-    signals = None
-
-    if args.print:
-        dp.print_data(data, title='Исходные данные')
+if __name__ == '__main__': 
+    parser = createParser() 
+    namespace = parser.parse_args(sys.argv[1:]) 
+    if namespace.file: 
+        logging.basicConfig(filename=namespace.file_log, level=namespace.level_log, filemode='w') 
+        logging.info('Программа запущена с параметрами ' + str(namespace) + " в режиме файл") 
+        try: 
+            process.process_file(namespace.file, namespace.up_value, namespace.file_out, namespace.indicator) 
+        except Exception as e: 
+            logging.error(str(e)) 
+    elif namespace.symbol: 
+        logging.basicConfig(filename=namespace.file_log, level=namespace.level_log) 
+        logging.info('Программа запущена с параметрами ' + str(namespace) + " в режиме сеть") 
+        try: 
+            process.process_network(namespace.symbol, namespace.up_value, namespace.year, namespace.file_out, namespace.indicator) 
+        except Exception as e: 
+            logging.error(str(e))
--- process.py
+++ process.py
@@ -1,83 +1,179 @@
-import logging
-from urllib.request import urlopen
-from urllib.parse import quote_plus
-import datetime
+import datetime 
+import csv 
+from urllib.request import urlopen 
+from urllib.parse import quote_plus 
+import logging 
 
-GOOGLE_FINANCE_URL = "http://www.google.com/finance/historical?" \
-                     "q={0}&startdate={1}&enddate={2}&output=csv"
+def day_value(info, max_value): 
+    day = 0 
+    for row_info in info:
+        if row_info > max_value: 
+            day = day + 1 
+    return day 
 
-
-def google(symbol, start, end):
-    """
-    Метод читает данные с сервиса Google Finance
-    Формат: Date,Open,High,Low,Close,Volume
-    :param symbol: символ
-    :param start: дата начала
-    :param end: дата конца
-    :return: словарь с полученными данными
-    """
-    url = GOOGLE_FINANCE_URL.format(
-        symbol.upper(),
-        quote_plus(start.strftime('%b %d, %Y')),
-        quote_plus(end.strftime('%b %d, %Y'))
-    )
-    # Делаем словарь для хранения наших данных в виде одного объекта
-    # Для хранения таких данных лучше подходят Pandas Data Frame
-    # http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html
+def day_MFI(data, window=10): 
+    try: 
+        import numpy as np
+        weight = np.ones((window,)) / window 
+        close = np.array(data['close']) 
+        low = np.array(data['low']) 
+        high = np.array(data['high']) 
+        volume = np.array(data['volume']) 
+        clv = ((close - low) - (high - close)) / (high - low) * volume 
+        mfi = [] 
+        pred_clv_i = 0 
+        for clv_i in clv: 
+            acc.append(pred_clv_i + clv_i)
+        weight = np.exp(np.linspace(-1., 0., window)) 
+        weight /= weight.sum() 
+        ema = np.convolve(mfi, weight)[:len(data['close'])]
+        ema = ema[window]: 
+        mfi = mfi[window]:
+    except ImportError: 
+        logging.error('Библиотека Numpy не найдена') 
+    dates = data['date'][window]:
     data = {
-        'date': [],
-        'open': [],
-        'close': [],
-        'high': [],
-        'low': [],
-        'volume': [],
-        'table': {},  # Для хранения данных по строкам
-        # Мета информация о наших колонках, нужна только для печати
-        'columns': ['open', 'close', 'high', 'low', 'volume']
-    }
-    raw_data = urlopen(url).readlines()
-    for line in raw_data[1:]:  # Пропускаем первую строку с именами колонок
-        row = line.decode().strip().split(',')
-        date = datetime.datetime.strptime(row[0], '%d-%b-%y').date()
-        open_price = float(row[1])
-        close_price = float(row[2])
-        high_price = float(row[3])
-        low_price = float(row[4])
-        volume_price = float(row[5])
-        data['date'].append(date)
-        data['open'].append(open_price)
-        data['close'].append(close_price)
-        data['high'].append(high_price)
-        data['low'].append(low_price)
-        data['volume'].append(volume_price)
-        data['table'][date] = {
-            'open': open_price,
-            'close': close_price,
-            'high': high_price,
-            'low': low_price,
-            'volume': volume_price,
+        'date': dates,
+        'ema': ema, 
+        'mfi': mfi, 
+        'table': {date: {'ema': ema[i], 'mfi': mfi[i]} for i, date in enumerate(dates)}, 
+        'columns': ['ema', 'mfi'] 
         }
-    return data
+    return data 
+
+def ema_signals(data, ema):
+    result = {}
+    pred_indicator_mfi = None 
+    pred_indicator_ema = None 
+    
+    for date in sorted(ema['table'].keys()): 
+        indicator_ema = ema['table'][date]['ema'] 
+        indicator_mfi = ema['table'][date]['mfi']
+        if pred_indicator_mfi == None: 
+            pred_indicator_mfi = indicator_mfi 
+            pred_indicator_ema = indicator_ema 
+        if (indicator_mfi > indicator_ema) and (pred_indicator_mfi < pred_indicator_ema):
+            result[date] = {'signal': 'BUY'} 
+        elif (indicator_mfi < indicator_ema) and (pred_indicator_mfi > pred_indicator_ema):
+            result[date] = {'signal': 'SELL'} 
+        pred_indicator_mfi = indicator_mfi 
+        pred_indicator_ema = indicator_ema 
+    return { 
+        'table': result, 
+        'text_columns': ['signal'] 
+    } 
+
+def print_file(file_out, rezult): 
+    if file_out: 
+        with open(file_out, 'w') as f_out: 
+            f_out.write(str(rezult)) 
+    print(rezult) 
+
+def print_file_signal(file_out, data, width=15):
+    row_format = '{date!s:^{width}}' 
+    if file_out: 
+        with open(file_out, 'w') as f_out: 
+            for date in sorted(data['table'].keys()): 
+                f_out.write(row_format.format(date=date, **data['table'][date], width=width) + data['table'][date]['signal'] + '\n') 
+                print(row_format.format(date=date, **data['table'][date], width=width) + data['table'][date]['signal']) 
+                
+
+def read_url(symbol, year): 
+    start = datetime.date(year, 1, 1) 
+    end = datetime.date(year, 12, 31) 
+    url = "http://www.google.com/finance/historical?q={0}&startdate={1}&enddate={2}&output=csv" 
+    url = url.format(symbol.upper(), quote_plus(start.strftime('%b %d, %Y')), quote_plus(end.strftime('%b %d, %Y'))) 
+    
+    data = urlopen(url).readlines() 
+    
+    info = { 
+        'date': [], 
+        'open': [], 
+        'close': [], 
+        'high': [], 
+        'low': [], 
+        'volume': [], 
+        'table': {}, # Для хранения данных по строкам 
+        #информация колонках 
+        'columns': ['open', 'close', 'high', 'low', 'volume'] 
+    } 
+    
+    for line in data[1:]: # Пропускаем первую строку с именами колонок 
+        row = line.decode().strip().split(',') 
+        date = datetime.datetime.strptime(row[0], '%d-%b-%y').date() 
+        open_price = float(row[1]) 
+        close_price = float(row[2]) 
+        high_price = float(row[3]) 
+        low_price = float(row[4]) 
+        volume_price = float(row[5])
+        info['date'].append(date) 
+        info['open'].append(open_price) 
+        info['close'].append(close_price) 
+        info['high'].append(high_price) 
+        info['low'].append(low_price) 
+        info['volume'].append(volume_price) 
+        info['table'][date] = { 
+            'open': open_price, 
+            'close': close_price, 
+            'high': high_price, 
+            'low': low_price, 
+            'volume': volume_price, 
+        } 
+        
+    return info 
+
+def read_file(file): 
+    info = { 
+        'date': [], 
+        'open': [], 
+        'close': [], 
+        'high': [], 
+        'low': [],
+        'volume': [], 
+        'table': {}, # Для хранения данных по строкам 
+        #информация колонках 
+        'columns': ['open', 'close', 'high', 'low', 'volume'] 
+    } 
+    with open(file) as f: 
+        f.readline() 
+        csv_file = csv.reader(f, delimiter=',') 
+        close_prices = [] 
+        for row in csv_file:
+            date = row[0] 
+            open_price = float(row[1]) 
+            close_price = float(row[4]) 
+            high_price = float(row[2]) 
+            low_price = float(row[3]) 
+            volume_price = float(row[5]) 
+            info['date'].append(date) 
+            info['open'].append(open_price) 
+            info['close'].append(close_price) 
+            info['high'].append(high_price) 
+            info['low'].append(low_price) 
+            info['volume'].append(volume_price) 
+            info['table'][date] = { 
+                'open': open_price, 
+                'close': close_price, 
+                'high': high_price, 
+                'low': low_price, 
+                'volume': volume_price, 
+                } 
+    return info 
 
 
-def print_data(data, width=15, title=''):
-    # Форматированный вывод исходнных данных, если пользователь указал флаг
-    columns_count = 1
-    head = 'DATE'.center(width)
+def process_data(data, up_value, file_out, indicator): 
+    if indicator is None: 
+        day = day_value(data["volume"], up_value) 
+        print_file(file_out, day) 
+    if indicator == "MFI": 
+        ema = day_MFI(data) 
+        signals = ema_signals(data, ema)
+        print_file_signal(file_out, signals) 
 
-    row_format = '{date!s:^{width}}'
-    if 'columns' in data:
-        columns_count += len(data['columns'])
-        for column in data['columns']:
-            head += column.upper().center(width)
-            row_format += '{' + column + ':^{width}.3f}'
-    if 'text_columns' in data:
-        columns_count += len(data['text_columns'])
-        for column in data['text_columns']:
-            head += column.upper().center(width)
-            row_format += '{' + column + ':^{width}}'
-    print(title.center(columns_count * width))
-    print(head.center(columns_count * width))
-    # Сортируем ключи и выводим
-    for date in sorted(data['table'].keys()):
-        print(row_format.format(date=date, **data['table'][date], width=width))
+def process_network(symbol, up_value, year, file_out, indicator): 
+    data = read_url(symbol, year) 
+    process_data(data, up_value, file_out, indicator) 
+
+def process_file(file, up_value, file_out, indicator): 
+    data = read_file(file) 
+    process_data(data, up_value, file_out, indicator)
